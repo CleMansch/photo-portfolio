33bf2cbe387b0aa70ca7b3c7c82e22ba90ea6aeb
unknown <calamanshi@gmx.at>
2021-03-01 16:21:37 +0100

    upload

---
+++ js/navbar.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$(".headerSection").hide();
+	console.log("CONNECTED");
+})
+
+function hideShow(){
+
+if(document.body.scrollTop>20 || document.documentElement.scrollTop > 20){
+	$(".headerSection").fadeIn(500);
+	console.log("SHOW");
+
+} else {
+	$(".headerSection").fadeOut(500);
+	console.log("HIDE");
+	
+}
+}
+
+
+$(function () {
+                var scroll = new SmoothScroll('a[href*="section-"]');
+
+        });
